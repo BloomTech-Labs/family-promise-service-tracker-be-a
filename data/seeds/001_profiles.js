9ab21b22e99e193b48b6bf8a13c2bb29576bd8a7
Chris <chris@databoxdigital.com>
2021-04-08 17:14:03 -0500

    updatees profiles to have roles

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,14 +1,35 @@
-const faker = require('faker');
-
-const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
-  avatarUrl: faker.image.avatar(),
-  email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
-  name:
-    idx === 0
-      ? 'Test001 User'
-      : `${faker.name.firstName()} ${faker.name.lastName()}`,
-}));
+const profiles = [
+  {
+    id: '00uk9lxaulDYOiB4H5d6',
+    email: 'bg_user@gmail.com',
+    name: 'bg_user basic',
+    role: 'unassigned',
+  },
+  {
+    id: '00ulthapbErVUwVJy4x6',
+    email: 'llama001@maildrop.cc',
+    name: 'Abigail Administrator',
+    role: 'administrator',
+  },
+  {
+    id: '00ulthapbasdfErVUwVJy4x6',
+    email: 'llama002@maildrop.cc',
+    name: 'Patty Program',
+    role: 'program_manager',
+  },
+  {
+    id: '00ulthaasdfgaspbErVUwVJy4x6',
+    email: 'llama003@maildrop.cc',
+    name: 'Sally Service',
+    role: 'service_provider',
+  },
+  {
+    id: '00ulthapbErgasfdsVUwVJy4x6',
+    email: 'llama004@maildrop.cc',
+    name: 'Gary Jerry',
+    role: 'unassigned',
+  },
+];
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
