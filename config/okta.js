9fc325e706d47b3e49102a8c6d2f9bd5f782e88f
Chris <chris@databoxdigital.com>
2021-04-14 16:19:13 -0500

    adds okta api configuration

--- config/okta.js
+++ config/okta.js
@@ -1,4 +1,11 @@
-module.exports = {
+const oktaClient = require('@okta/okta-sdk-nodejs');
+
+const okta = new oktaClient.Client({
+  orgUrl: process.env.OKTA_ORG_URL,
+  token: process.env.OKTA_API_TOKEN,
+});
+
+const oktaVerifierConfig = {
   expectedAudience: ['api://default', `${process.env.OKTA_CLIENT_ID}`],
   config: {
     issuer: `${process.env.OKTA_URL_ISSUER}`,
@@ -7,4 +14,9 @@ module.exports = {
       aud: `${process.env.OKTA_CLIENT_ID}`,
     },
   },
+};
+
+module.exports = {
+  okta,
+  oktaVerifierConfig,
 };
