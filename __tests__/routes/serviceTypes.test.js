31957413c1e72dbf144c76ca9b4f7258242f5db9
Chris <chris@databoxdigital.com>
2021-04-21 17:06:10 -0500

    adds test file for service types

---
+++ __tests__/routes/serviceTypes.test.js
@@ -0,0 +1,90 @@
+const request = require('supertest');
+const express = require('express');
+const ServiceTypes = require('../../api/serviceTypes/serviceTypeModel');
+const DB = require('../../api/utils/db-helper');
+const serviceTypeRouter = require('../../api/serviceTypes/serviceTypeRouter');
+const server = express();
+server.use(express.json());
+
+jest.mock('../../api/utils/db-helper');
+jest.mock('../../api/serviceTypes/serviceTypeModel');
+
+// mock the auth middlewares completely
+jest.mock('../../api/middleware/authRequired', () =>
+  jest.fn((req, res, next) => next())
+);
+
+jest.mock('../../api/middleware/authorization', () => ({
+  requireAdmin: (req, res, next) => next(),
+  canEditProfile: (req, res, next) => next(),
+}));
+
+describe('service types router endpoints', () => {
+  beforeAll(() => {
+    // This is the module/route being tested
+    server.use(['/service_type', '/service_types'], serviceTypeRouter);
+    jest.clearAllMocks();
+  });
+
+  describe('GET /service_types', () => {
+    it('should return 200', async () => {
+      ServiceTypes.findAll.mockResolvedValue([]);
+      const res = await request(server).get('/service_types');
+
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(0);
+      expect(ServiceTypes.findAll.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('GET /service_types/:id', () => {
+    it('should return 200 when service type found', async () => {
+      ServiceTypes.findById.mockResolvedValue({
+        id: '1',
+        name: 'Bus Passes',
+        description: 'Monthly allowance for bus pass',
+      });
+      const res = await request(server).get('/service_types/1');
+
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe('Bus Passes');
+      expect(ServiceTypes.findById.mock.calls.length).toBe(1);
+    });
+
+    it('should return 404 when no service type found', async () => {
+      ServiceTypes.findById.mockResolvedValue();
+      const res = await request(server).get('/service_types/245');
+
+      expect(res.status).toBe(404);
+      expect(res.body.error).toBe('Service Type 245 not found');
+    });
+  });
+
+  describe('POST /service_type', () => {
+    it('should return 201 with created service type', async () => {
+      const service = {
+        name: 'Bus Pass',
+        description: 'Monthly bus pass allowance',
+        program: 1,
+      };
+      ServiceTypes.create.mockResolvedValue(service);
+      const res = await request(server).post('/service_type').send(service);
+      expect(res.status).toBe(201);
+      expect(res.body.message).toMatch(/New service type created/);
+      expect(ServiceTypes.create.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('PUT /service_type/:id', () => {
+    it('should return 200 when service type is updated', async () => {
+      const service = { name: 'Rental Assistance' };
+      DB.findById.mockResolvedValue(service);
+      ServiceTypes.update.mockResolvedValue(service);
+
+      const res = await request(server).put('/service_types/1').send(service);
+      expect(res.status).toBe(200);
+      expect(res.body.service_type.name).toBe('Rental Assistance');
+      expect(ServiceTypes.update.mock.calls.length).toBe(1);
+    });
+  });
+});
