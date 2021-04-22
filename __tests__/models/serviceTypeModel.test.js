5dd07559d013c66e7616169c670148770d5c26ad
Chris <chris@databoxdigital.com>
2021-04-21 23:35:10 -0500

    adds tests for service type model

--- __tests__/models/serviceTypeModel.test.js
+++ __tests__/models/serviceTypeModel.test.js
@@ -1,19 +1,55 @@
-const ServiceTypeModel = require('../../api/serviceTypes/serviceTypeModel');
-//const knex = require('../../data/db-config');
+const ServiceType = require('../../api/serviceTypes/serviceTypeModel');
 
 describe('Service Type Model Methods', () => {
   beforeAll(async () => {
     jest.clearAllMocks();
   });
 
-  describe('Find All Service Types', () => {
+  afterAll(async () => {
+    // kill the database connection after tests
+    // have run to make sure Jest can exit
+    ServiceType.knex.destroy();
+  });
+
+  describe('ServiceType.findAll', () => {
     it('should return 3 records', async () => {
-      try {
-        const rows = await ServiceTypeModel.findAll();
-        expect(rows.length).toBe(3);
-      } catch (e) {
-        console.warn(e);
-      }
+      const rows = await ServiceType.findAll();
+      expect(rows.length).toBe(3);
+    });
+  });
+
+  describe('ServiceType.findById', () => {
+    it('should return correct record when given valid ID', async () => {
+      const serviceType = await ServiceType.findById(1);
+      expect(serviceType.name).toBe('Bus Passes');
+      expect(serviceType.service_providers.length).toBe(2);
+    });
+    it('should return no records when given invalid ID', async () => {
+      const serviceType = await ServiceType.findById(5);
+      expect(serviceType).toBe(undefined);
+    });
+  });
+
+  describe('ServiceType.create', () => {
+    it('should create new service type when valid', async () => {
+      // ensure we have right number of records in DB to start
+      const startRecords = await ServiceType.findAll();
+      expect(startRecords.length).toBe(3);
+
+      // then create new service type
+      const newRecord = await ServiceType.create({
+        name: 'Brand New Service',
+        description: 'A good service for good people',
+        program_id: 1,
+        service_providers: ['22ulthapbErVUwVJy2x2'],
+      });
+
+      // test the returned object
+      expect(newRecord.name).toBe('Brand New Service');
+      expect(newRecord.service_providers.length).toBe(1);
+      // test that we've correctly added to DB
+      const allRecords = await ServiceType.findAll();
+      expect(allRecords.length).toBe(4);
     });
   });
 });
