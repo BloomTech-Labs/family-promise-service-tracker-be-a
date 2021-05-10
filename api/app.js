e7115321738ceb4ba8e3f2ed1bcd60920bcb30fe
Jennifer Kramer <jenkrame@gmail.com>
2021-05-10 19:31:19 -0400

    Disabled lines in recipientModel and recipientRouter.

--- api/app.js
+++ api/app.js
@@ -26,6 +26,7 @@ const statusRouter = require('./statuses/statusesRouter');
 const serviceTypeRouter = require('./serviceTypes/serviceTypeRouter');
 const serviceEntryRouter = require('./serviceEntries/serviceEntriesRouter');
 const dsRouter = require('./dsService/dsRouter');
+const recipientRouter = require('./recipient/recipientRouter');
 
 const app = express();
 
@@ -60,6 +61,7 @@ app.use(['/api/status', '/api/statuses'], statusRouter);
 app.use(['/api/service_type', '/api/service_types'], serviceTypeRouter);
 app.use(['/api/service_entry', '/api/service_entries'], serviceEntryRouter);
 app.use('/data', dsRouter);
+app.use(['/api/recipient', '/api/recipients'], recipientRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ api/recipient/recipientRouter.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// eslint-disable-next-line
+const DB = require('../utils/db-helper');
+// eslint-disable-next-line
+const Recipients = require('./recipientModel');
+const router = express.Router();
+// Need to make middleware for recipients
+
+// eslint-disable-next-line
+router.get('/', (req, res) => {
+
+});
+
+// eslint-disable-next-line
+router.get('/:id', (req, res) => {
+
+});
+
+// eslint-disable-next-line
+router.post('/', (req, res) => {
+    
+});
+
+// eslint-disable-next-line
+router.put('/:id', (req, res) => {
+
+});
+
+// eslint-disable-next-line
+router.delete('/:id', (req, res) => {
+
+});
+
+// eslint-disable-next-line
+module.exports = router;
