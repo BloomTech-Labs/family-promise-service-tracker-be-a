5f39a29597b047b867b046dc71dbc18f81d701b7
taratimmerman <timmermantara@gmail.com>
2021-05-11 14:49:55 -0600

    Filled out PUT and POST endpoint in recipientRouter.js, fixed prettier CR error

--- api/recipient/recipientRouter.js
+++ api/recipient/recipientRouter.js
@@ -1,14 +1,18 @@
 const express = require('express');
 // eslint-disable-next-line
 const DB = require('../utils/db-helper');
-// eslint-disable-next-line
-const Recipients = require('./recipientModel');
 const router = express.Router();
 // Need to make middleware for recipients
 
 // eslint-disable-next-line
 router.get('/', (req, res) => {
-
+  DB.findAll('recipients')
+    .then((recipients) => {
+      res.status(200).json(recipients);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // eslint-disable-next-line
@@ -16,14 +20,27 @@ router.get('/:id', (req, res) => {
 
 });
 
-// eslint-disable-next-line
 router.post('/', (req, res) => {
-    
+  DB.create('recipients', req.body)
+    .then((newRecipient) => {
+      res.status(201).json({ message: 'Recipient created', newRecipient });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
-// eslint-disable-next-line
 router.put('/:id', (req, res) => {
-
+  DB.update('recipients', req.params.id, req.body)
+    .then((editedRecipient) => {
+      res.status(200).json({
+        message: `Recipient ${req.params.id} updated`,
+        editedRecipient,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // eslint-disable-next-line
