5f94016a03cc66c8ce2b2b317aa6c6703bba19c5
Nathan Hietala <Nrhietala@gmail.com>
2021-05-11 20:29:14 -0500

    adds endpoint to specifically grab service providers from profiles table

--- api/profile/profileRouter.js
+++ api/profile/profileRouter.js
@@ -172,6 +172,16 @@ router.get('/', function (req, res) {
  *      404:
  *        description: 'Profile not found'
  */
+
+router.get('/getServiceProviders', function (req, res) {
+  Profiles.findServiceProviders()
+    .then((serviceProviders) => {
+      res.status(200).json(serviceProviders);
+    })
+    .catch((err) => {
+      res.status(404).json({ error: 'No Service Providers Found' });
+    });
+});
 router.get('/:id', function (req, res) {
   const id = String(req.params.id);
   Profiles.findById(id)
