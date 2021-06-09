925e0c8c8d354e80cd77e2cc0202142eddeac3eb
SpicyLunchbox <westonwoodard28@gmail.com>
2021-06-09 13:41:44 -0700

    email_addresses and service_entry_recipients tables complete

---
+++ data/newMigrations/015-service_entry_recipients.js
@@ -0,0 +1,26 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('service_entry_recipients', (tbl) => {
+    tbl.increments('service_entry_provider_id').primary();
+    tbl
+      .integer('service_entry_id')
+      .notNullable()
+      .unsigned()
+      .references('service_entry_id')
+      .inTable('service_entries')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('provider_id')
+      .notNullable()
+      .unsigned()
+      .references('provider_id')
+      .inTable('providers')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('service_entry_recipients');
+};
