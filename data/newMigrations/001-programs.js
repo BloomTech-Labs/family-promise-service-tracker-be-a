610d92855c21a1dd74fc7122714c3044a241de0d
Domenic Scarcella <ls41513@yahoo.com>
2021-06-09 14:24:10 -0400

    mew programs table in newMigrations folder

---
+++ data/newMigrations/001-programs.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+exports.up = function (knex) {
+    return knex.schema.createTable('programs', tbl => {
+        tbl.increments('program_id').primary();
+        tbl.string('program_name', 255).notNullable().unique();
+        tbl.string('program_description', 1000).notNullable();
+        tbl.timestamp('created_at').defaultTo(knex.fn.now());
+        tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('programs');
+};
