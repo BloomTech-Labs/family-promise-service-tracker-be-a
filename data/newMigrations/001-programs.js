a91e0c07c69a0eff5f37a36db1a32d5e118ee434
Domenic Scarcella <ls41513@yahoo.com>
2021-06-09 17:13:04 -0400

    cleaned code for style

--- data/newMigrations/001-programs.js
+++ data/newMigrations/001-programs.js
@@ -1,13 +1,16 @@
-/* eslint-disable prettier/prettier */
 exports.up = function (knex) {
     return knex.schema.createTable('programs', tbl => {
-        tbl.increments('program_id').primary();
-        tbl.string('program_name', 255).notNullable().unique();
-        tbl.string('program_description', 1000).notNullable();
+        tbl.increments('program_id')
+            .primary();
+        tbl.string('program_name', 255)
+            .notNullable()
+            .unique();
+        tbl.string('program_description', 1000)
+            .notNullable();
         tbl.timestamps(true, true);
     });
 };
-  
+
 exports.down = function (knex) {
     return knex.schema.dropTableIfExists('programs');
 };
