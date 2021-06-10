5bac7b9bdd55e095db5c9fa8412d1e711d8e516e
Domenic Scarcella <ls41513@yahoo.com>
2021-06-09 22:37:37 -0400

    Some .text tweaks and .string reinserted

--- data/newMigrations/001-programs.js
+++ data/newMigrations/001-programs.js
@@ -1,10 +1,11 @@
+/* eslint-disable prettier/prettier */
 exports.up = function (knex) {
     return knex.schema.createTable('programs', tbl => {
         tbl.increments('program_id');
         tbl.text('program_name', 255)
             .notNullable()
             .unique();
-        tbl.text('program_description', 1000)
+        tbl.text('program_description')
             .notNullable();
         tbl.timestamps(true, true);
     });
--- data/newMigrations/003-service_categories.js
+++ data/newMigrations/003-service_categories.js
@@ -1,10 +1,11 @@
+/* eslint-disable prettier/prettier */
 exports.up = function (knex) {
     return knex.schema.createTable('service_categories', tbl => {
         tbl.increments('service_category_id');
-        tbl.text('service_category_name', 255)
+        tbl.string('service_category_name', 255)
             .notNullable()
             .unique();
-        tbl.text('service_category_description', 1000);
+        tbl.text('service_category_description');
         tbl.timestamps(true, true);
     });
 };
