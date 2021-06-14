17a87b9a24a4d0e793fdd65508e1322dbdb3c7ca
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-06-14 17:04:53 -0600

    update locations migration to fix issue

--- data/migrations/004-locations.js
+++ data/migrations/004-locations.js
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable('locations', function (tbl) {
-    tbl.increments('location_id').primary();
+    tbl.increments('location_id');
     tbl.string('location_name').notNullable();
-    tbl.string('location_description').notNullable();
-    tbl.string('address').notNullable();
-    tbl.string('address_line2').notNullable();
+    tbl.text('location_description').notNullable();
+    tbl.text('address').notNullable();
+    tbl.text('address_line2');
     tbl.string('city').notNullable();
     tbl.string('state').notNullable();
     tbl.string('zip').notNullable();
