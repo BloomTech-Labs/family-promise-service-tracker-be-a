9bf467c1aeda81ae8efe6debfe02a91e82a43a9a
MatthewEllisTx <mbellistx@gmail.com>
2021-06-15 01:52:18 -0500

    013-recipients.js mostly done. need household uuid

---
+++ data/seeds/013-recipients.js
@@ -0,0 +1,42 @@
+const recipients = [
+  {
+    recipient_id: 'ec6f7c16-d064-4ae3-a759-4fea047f2c76',
+    recipient_first_name: 'bob',
+    recipient_middle_name: 'tree',
+    recipient_last_name: 'ross',
+    recipient_date_of_birth: '1970-10-12',
+    recipient_veteran_status: false,
+    household_id: 'x', //need uuid
+    gender_id: 1,
+    rade_id: 1,
+    ethnicity_id: 1
+  },
+  {
+    recipient_id: '81a40c69-b004-4cc8-b6a5-424cbab89b8d',
+    recipient_first_name: 'bob',
+    recipient_middle_name: 'tree',
+    recipient_last_name: 'ross',
+    recipient_date_of_birth: '1970-10-12',
+    recipient_veteran_status: false,
+    household_id: 'x', //need uuid
+    gender_id: 1,
+    rade_id: 1,
+    ethnicity_id: 1
+  },
+  {
+    recipient_id: '1b1639f9-a614-4d15-93d8-1d0b27ca12a6',
+    recipient_first_name: 'bob',
+    recipient_middle_name: 'tree',
+    recipient_last_name: 'ross',
+    recipient_date_of_birth: '1970-10-12',
+    recipient_veteran_status: false,
+    household_id: 'x', //need uuid
+    gender_id: 1,
+    rade_id: 1,
+    ethnicity_id: 1
+  },
+];
+
+exports.seed = function (knex) {
+  return knex('recipients').insert(recipients);
+};
