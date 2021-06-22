fb0cc14b723aef98a842ba69c69552b75443e6a2
MatthewEllisTx <mbellistx@gmail.com>
2021-06-22 15:25:09 -0500

    removed findByName, changed it to new findAll

--- api/recipient/recipientModel.js
+++ api/recipient/recipientModel.js
@@ -18,33 +18,6 @@ const findAll = (filter = {}) => {
     .where(filter);
 };
 
-const findByName = async (firstName, middleName, lastName) => {
-  return await knex('recipients')
-    .leftJoin('households', {
-      'recipients.household_id': 'households.household_id',
-    })
-    .leftJoin('genders', {
-      'recipients.gender_id': 'genders.gender_id',
-    })
-    .leftJoin('races', {
-      'recipients.race_id': 'races.race_id',
-    })
-    .leftJoin('ethnicities', {
-      'recipients.ethnicity_id': 'ethnicities.ethnicity_id',
-    })
-    .select(
-      knex.raw(
-        'recipients.*, genders.* as gender, races.* as race, ethnicities.* as ethnicity'
-      )
-    )
-    .where({
-      'recipients.recipient_first_name': firstName,
-      'recipients.recipient_middle_name': middleName,
-      'recipients.recipient_last_name': lastName,
-    });
-};
-
 module.exports = {
-  findByName,
-  findAll,
+  findAll
 };
