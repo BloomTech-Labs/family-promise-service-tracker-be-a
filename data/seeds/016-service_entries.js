90ba3a90c8f94351cb2bf28e4a955e4a0d5603c7
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-06-23 12:03:27 -0600

    Updated formatting of entry_data to match updated doc

--- data/seeds/016-service_entries.js
+++ data/seeds/016-service_entries.js
@@ -9,7 +9,7 @@ const {
 const { service_types } = require('./006-service_types');
 
 const service_entries = fakeServiceEntryIds.map((id) => {
-  const serviceTypeNumToUse = getRand(service_types.length - 1);
+  const serviceTypeNumToUse = getRand(service_types.length - 1); // Using '- 1' since we don't want to include service_type_id = 999, which isn't a real service_type
   return {
     service_entry_id: id,
     service_type_id: serviceTypeNumToUse,
@@ -17,20 +17,16 @@ const service_entries = fakeServiceEntryIds.map((id) => {
     service_date: faker.date.past(3),
     service_time: faker.time.recent('abbr'),
     service_entry_data: {
-      default: [
-        { 'Cost/Value': getRandWithZero(80) },
-        { Duration: getRand(120) },
-        {
-          Notes: faker.lorem.paragraph(),
-        },
-        {
-          Status: ['Completed', 'Pending', 'In Progress', 'Follow Up Required'][
-            getRandWithZero(4)
-          ],
-        },
-        { 'Success Rating': getRandWithZero(6) },
-      ],
-      custom: fakeServiceEntryTypeCustomData(serviceTypeNumToUse),
+      default: {
+        'Cost/Value': getRandWithZero(80),
+        Duration: getRand(116) + 4, // So duration will always be at least 5 minutes
+        Notes: faker.lorem.paragraph(),
+        Status: ['Completed', 'Pending', 'In Progress', 'Follow Up Required'][
+          getRandWithZero(4)
+        ],
+        'Success Rating': getRandWithZero(6),
+      },
+      custom: fakeServiceEntryTypeCustomData(serviceTypeNumToUse - 1), // In this case using '-1' since the fake service creator is zero indexed
     },
   };
 });
@@ -40,135 +36,119 @@ exports.seed = function (knex) {
 };
 
 function fakeServiceEntryTypeCustomData(entryNum) {
+  let aller;
+
   return [
-    [
-      {
-        'Bus Service Provider': [
-          'Spokane Transit Authority',
-          'InterCity Transit',
-          'FlixBus',
-          'Greyhound',
-        ][getRandWithZero(4)],
-      },
-    ],
-    [
-      {
-        'Date rent is due': faker.date.soon(),
-      },
-      {
-        'Is rental assistance expected to be required again next month?': [
-          'Yes',
-          'No',
-          'Unclear',
-        ][getRandWithZero(3)],
-      },
-    ],
-    [
-      {
-        'Food for what amount of persons?': getRand(8),
-      },
-      {
-        'Food Allergies?': (function () {
-          const allergyLotto = getRandWithZero(10);
-          const output = [];
-          if (allergyLotto <= 3) {
-            output.push('none');
-          } else if (3 < allergyLotto <= 5) {
-            output.push('Shellfish');
-          } else if (5 < allergyLotto <= 7) {
-            output.push('Shellfish', 'Peanuts');
-          } else if (7 < allergyLotto <= 9) {
-            output.push('Gluten');
-          } else {
-            output.push('Peanuts', 'Gluten', 'Milk', 'Eggs', 'Other');
-          }
-          return output;
-        })(),
-      },
-      {
-        'If other allergy, what allergy?': (function () {
-          const specialAllergy = getRand(100);
-          if (specialAllergy > 80) {
-            return 'Red Meat Protein';
-          }
-        })(),
-      },
-    ],
-    [
-      {
-        'Bus Service Provider': [
-          'Spokane Transit Authority',
-          'InterCity Transit',
-          'FlixBus',
-          'Greyhound',
-        ][getRandWithZero(4)],
-      },
-      {
-        'Bus Pass Start Date': faker.date.past(),
-      },
-      {
-        'Bus Pass End Date': faker.date.future(),
-      },
-    ],
-    [
-      {
-        'Visible emotional state of client': [
-          'Normal',
-          'Excited',
-          'Agitated',
-          'Apathetic',
-          'Distressed',
-          'Depressed',
+    {
+      'Bus Service Provider': [
+        'Spokane Transit Authority',
+        'InterCity Transit',
+        'FlixBus',
+        'Greyhound',
+      ][getRandWithZero(4)],
+    },
+    {
+      'Date rent is due': (function () {
+        const date = faker.date.soon();
+        return date;
+      })(),
+      'Is rental assistance expected to be required again next month?': [
+        'Yes',
+        'No',
+        'Unclear',
+      ][getRandWithZero(3)],
+    },
+    {
+      'Food for what amount of persons?': getRand(8),
+      'Food Allergies?': (function () {
+        const allergyOptions = [
+          'None',
+          'Shellfish',
+          'Peanuts',
+          'Gluten',
+          'Milk',
+          'Eggs',
           'Other',
-        ][getRandWithZero(7)],
-      },
-      {
-        'Is follow up be necessary?': faker.datatype.boolean(),
-      },
-    ],
-    [
-      {
-        'Which life skills were covered?': (function () {
-          const options = [
-            'Budgeting',
-            'Time Management',
-            'Interpersonal Skills',
-            'Meal Planning',
-            'Resume Prep',
-            'Filing Taxes',
-            'Other',
-          ];
-          const output = [];
-
-          options.forEach((skill) => {
-            if (getRand(10) > 5) {
-              output.push(skill);
+        ];
+        const output = [];
+        allergyOptions.forEach((alg) => {
+          if (output.includes('None')) {
+            aller = 'None';
+            return;
+          } else if (getRand(10) > 8) {
+            if (alg === 'Other') {
+              aller = alg;
             }
-          });
+            output.push(alg);
+          }
+        });
+        if (output.length === 0) {
+          aller = 'None';
+          output.push('None');
+        }
+        return output;
+      })(),
+      'If other allergy, what allergy?': (function () {
+        if (aller === 'Other') {
+          return 'Red Meat Protein';
+        }
+      })(),
+    },
+    {
+      'Bus Service Provider': [
+        'Spokane Transit Authority',
+        'InterCity Transit',
+        'FlixBus',
+        'Greyhound',
+      ][getRandWithZero(4)],
+      'Bus Pass Start Date': faker.date.past(),
+      'Bus Pass End Date': faker.date.future(),
+    },
+    {
+      'Visible emotional state of client': [
+        'Normal',
+        'Excited',
+        'Agitated',
+        'Apathetic',
+        'Distressed',
+        'Depressed',
+        'Other',
+      ][getRandWithZero(7)],
+      'Is follow up be necessary?': faker.datatype.boolean(),
+    },
+    {
+      'Which life skills were covered?': (function () {
+        const options = [
+          'Budgeting',
+          'Time Management',
+          'Interpersonal Skills',
+          'Meal Planning',
+          'Resume Prep',
+          'Filing Taxes',
+          'Other',
+        ];
+        const output = [];
 
-          return output;
-        })(),
-      },
-      {
-        'Will follow up sessions be required?': faker.datatype.boolean(),
-      },
-    ],
-    [
-      {
-        'What was the security deposit for?': [
-          'Apartment',
-          'Vehicle',
-          'Electric Utility',
-          'Water Utility',
-          'Other Utility',
-        ][getRandWithZero(5)],
-      },
-      {
-        'Refundable security deposit?': faker.datatype.boolean(),
-      },
-      {
-        'Security deposit due date': faker.date.future(),
-      },
-    ],
+        options.forEach((skill) => {
+          if (getRand(10) > 7) {
+            output.push(skill);
+          }
+        });
+
+        return output;
+      })(),
+      'Will follow up sessions be required?': faker.datatype.boolean(),
+    },
+    {
+      'What was the security deposit for?': [
+        'Apartment',
+        'Vehicle',
+        'Electric Utility',
+        'Water Utility',
+        'Other Utility',
+      ][getRandWithZero(5)],
+      'Refundable security deposit?': faker.datatype.boolean(),
+      'Security deposit due date': faker.date.future(),
+    },
   ][entryNum];
 }
