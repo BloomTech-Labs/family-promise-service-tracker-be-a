d54d4ed7fcb98fc1559159133fbcb99a59cb2a04
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-06-22 18:29:29 -0600

    Altered service_type_cats for additional types

--- data/seeds/008-service_type_categories.js
+++ data/seeds/008-service_type_categories.js
@@ -8,8 +8,28 @@ const service_type_categories = [
     service_type_id: 2,
   },
   {
-    service_category_id: 3,
+    service_category_id: 4,
     service_type_id: 3,
+  },
+  {
+    service_category_id: 1,
+    service_type_id: 4,
+  },
+  {
+    service_category_id: 6,
+    service_type_id: 5,
+  },
+  {
+    service_category_id: 5,
+    service_type_id: 6,
+  },
+  {
+    service_category_id: 2,
+    service_type_id: 7,
+  },
+  {
+    service_category_id: 3,
+    service_type_id: 7,
   },
 ];
 
