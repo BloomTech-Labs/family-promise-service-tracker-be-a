7c3c7b56def92687b397cabb93c46195284ad477
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-19 14:30:15 -0400

    updated househlds tbl

--- data/migrations/015-households.js
+++ data/migrations/015-households.js
@@ -4,10 +4,6 @@ exports.up = function (knex) {
       .uuid('household_id')
       .primary()
       .defaultTo(knex.raw('gen_random_uuid ()'));
-    tbl.string('household_name', 255).notNullable();
-    tbl.boolean('is_unstable').notNullable().defaultTo(false);
-    tbl.integer('household_size').notNullable();
-    tbl.integer('household_income').notNullable();
     tbl
       .integer('location_id')
       .notNullable()
@@ -16,6 +12,10 @@ exports.up = function (knex) {
       .inTable('locations')
       .onUpdate('CASCADE')
       .onDelete('CASCADE');
+    tbl.string('household_name', 255).notNullable();
+    tbl.integer('household_size').defaultTo(1);
+    tbl.decimal('household_monthly_income');
+    tbl.boolean('is_unstable').notNullable().defaultTo(false);
     tbl.timestamps(true, true);
   });
 };
@@ -23,3 +23,5 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTableIfExists('households');
 };
+
+// Multiple recipients can be a part of a household, and all the occupants of a household can be the recipients of a particular logged service. household_size refers to the current number of occupants of a household. A household_id must be associated with a recipient, even if that recipient is doesn't have an address currently. b/c of this, location_id is nullable. Household describes the social unit, not the physical location.
