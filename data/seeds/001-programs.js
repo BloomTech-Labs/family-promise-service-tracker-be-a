49f8663a526a1b5c5a8dceb1243b353e4b3fe0fd
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-19 15:10:56 -0400

    pr seed update

--- data/seeds/001-programs.js
+++ data/seeds/001-programs.js
@@ -3,16 +3,19 @@ const programs = [
     program_name: 'Shelter Support',
     program_description:
       'Serving the currently homeless: We work to help members of our community stay who have lost their homes.',
+    program_is_active: true,
   },
   {
     program_name: 'Prevention/Diversion',
     program_description:
       'Preventing Homelessness: We work to help members of our community stay in their homes and not become homeless.',
+    program_is_active: true,
   },
   {
     program_name: 'Aftercare',
     program_description:
       'Supporting families after becoming renters/homeowners again to prevent ending up homeless again: We work to help members of our community who have previously been homeless, but no longer are.',
+    program_is_active: true,
   },
 ];
 
