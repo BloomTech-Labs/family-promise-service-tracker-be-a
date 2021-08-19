665793d7663e5a10c41f42111bd83e968b51089d
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-08-19 12:23:41 -0500

    Skeletonized houshold model/router removed db-help

--- api/household/householdModel.js
+++ api/household/householdModel.js
@@ -1,44 +1,19 @@
 const knex = require('../../data/db-config');
 
-const create = async (households) => {
-  let newHouseholdId;
-  try {
-    await knex.transaction(async (trx) => {
-      const createdHousehold = await trx('households')
-        .insert([
-          { household_id: households.household_id },
-          { location_id: households.location_id },
-          { household_name: households.household_name },
-          { household_size: households.household_size },
-          { household_income: households.household_income },
-        ])
-        .returning('*');
-      newHouseholdId = createdHousehold[0].household_id;
-    });
-    return await findById(newHouseholdId);
-  } catch (err) {
-    throw new Error(err);
-  }
-};
+const findAll = () => {};
 
-const findById = async (id) => {
-  return await knex('households')
-    .where({ 'households.household_id': id })
-    .leftJoin('locations', {
-      'households.location_id': 'locations.location_id',
-    })
-    .select(
-      knex.raw(
-      'households.*, to_json(locations.*) as location'
-      )
-    )
-    .first()
-    .groupBy(
-      'households.household_id','locations.location_id'
-    );
-};
+const findById = async () => {};
+
+const create = async () => {};
+
+const update = async () => {};
+
+const remove = () => {};
 
 module.exports = {
-  create,
+  findAll,
   findById,
+  create,
+  update,
+  remove,
 };
--- api/household/householdRouter.js
+++ api/household/householdRouter.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const DB = require('../utils/db-helper');
 const router = express.Router();
 const Households = require('./householdModel');
 const { requireAdmin } = require('../middleware/authorization');
 
 // GET - View all households
 router.get('/', (req, res, next) => {
-  DB.findAll('households')
+  Households.findAll()
     .then((households) => {
       res.status(200).json(households);
     })
@@ -17,7 +16,7 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
 
-  DB.findById('households', id)
+  Households.findById()
     .then((household) => {
       if (household) {
         res.status(200).json(household);
@@ -30,7 +29,7 @@ router.get('/:id', (req, res, next) => {
 
 // POST - Create new household
 router.post('/', (req, res, next) => {
-  Households.create('households', req.body)
+  Households.create()
     .then((response) => {
       return Households.findById(response[0].household_id);
     })
@@ -42,7 +41,7 @@ router.post('/', (req, res, next) => {
 
 // PUT - Update household by ID
 router.put('/:id', (req, res, next) => {
-  DB.update('households', req.params.id, req.body)
+  Households.update()
     .then((editedHousehold) => {
       res.status(200).json({
         message: `Household ${req.params.id} updated`,
@@ -56,7 +55,7 @@ router.put('/:id', (req, res, next) => {
 router.delete('/:id', requireAdmin, (req, res, next) => {
   const { id } = req.params;
 
-  DB.remove('households', id)
+  Households.remove()
     .then((count) => {
       if (count > 0) {
         res.status(200).json({ message: `Household ${id} has been removed` });
