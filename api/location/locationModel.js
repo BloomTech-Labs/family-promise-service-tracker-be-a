5d20b0da158d72d9a2b2d36526d2d53de97c3f61
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-08-19 14:39:21 -0500

    Created skeletonized location model, changed references from db-helper to model within location-router

---
+++ api/location/locationModel.js
@@ -0,0 +1,19 @@
+const knex = require('../../data/db-config');
+
+const findAll = () => {};
+
+const findById = async () => {};
+
+const create = async () => {};
+
+const update = async () => {};
+
+const remove = () => {};
+
+module.exports = {
+  findAll,
+  findById,
+  create,
+  update,
+  remove,
+};
