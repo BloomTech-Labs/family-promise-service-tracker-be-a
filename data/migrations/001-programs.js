6f2fa009e0385039f4eed6d009f9ab92fb35dc64
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-19 12:56:42 -0400

    program tbl updated

--- data/migrations/001-programs.js
+++ data/migrations/001-programs.js
@@ -1,8 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('programs', (tbl) => {
     tbl.increments('program_id');
-    tbl.string('program_name', 255).notNullable().unique();
+    tbl.string('program_name', 255).notNullable();
     tbl.text('program_description').notNullable();
+    tbl.boolean('program_is_active').notNullable().defaultTo(true);
     tbl.timestamps(true, true);
   });
 };
