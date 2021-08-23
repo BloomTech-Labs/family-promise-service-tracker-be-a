bdc943c15ae70cae611fdf21cf7cf67e15c5a05d
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-23 17:26:43 -0400

    updated recip model

--- api/recipient/recipientModel.js
+++ api/recipient/recipientModel.js
@@ -1,11 +1,21 @@
 const knex = require('../../data/db-config');
 
 const findAll = async () => {
-  return await knex('recipients');
+  return await knex('recipients as r')
+    .join('genders as g', 'g.gender_id', 'r.gender_id')
+    .join('races as ra', 'ra.race_id', 'r.race_id')
+    .join('ethnicities as e', 'e.ethnicity_id', 'r.ethnicity_id')
+    .select('*');
 };
 
 const findById = async (id) => {
-  return await knex('recipients').where('recipient_id', id).first();
+  return await knex('recipients as r')
+    .join('genders as g', 'g.gender_id', 'r.gender_id')
+    .join('races as ra', 'ra.race_id', 'r.race_id')
+    .join('ethnicities as e', 'e.ethnicity_id', 'r.ethnicity_id')
+    .select('*')
+    .where('recipient_id', id)
+    .first();
 };
 
 const createRecipient = async (newRecipient) => {
