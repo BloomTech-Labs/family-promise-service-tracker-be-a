3fb5567051e50809fe35a8985eee82a7015f454e
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-24 16:52:19 -0400

    updated final endpoints

--- api/providerRoles/providerRoleRouter.js
+++ api/providerRoles/providerRoleRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ProviderRoles = require('./statusModel');
+const ProviderRoles = require('./providerRoleModel');
 const { requireAdmin } = require('../middleware/authorization');
 
 router.get('/', (req, res, next) => {
