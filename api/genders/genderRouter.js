9839a459758615cc4f24abb77c1a084983d5742c
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-24 13:08:53 -0400

    updated gender endpoints

--- api/genders/genderRouter.js
+++ api/genders/genderRouter.js
@@ -1,44 +1,44 @@
 const express = require('express');
-const Statuses = require('./statusModel');
+const Genders = require('./genderModel');
 const router = express.Router();
 const { requireAdmin } = require('../middleware/authorization');
 
 router.get('/', (req, res, next) => {
-  Statuses.findAll()
-    .then((statuses) => {
-      res.status(200).json(statuses);
+  Genders.findAll()
+    .then((genders) => {
+      res.status(200).json(genders);
     })
     .catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
-  Statuses.findById(id)
-    .then((status) => {
-      if (status) {
-        res.status(200).json(status);
+  Genders.findById(id)
+    .then((gender) => {
+      if (gender) {
+        res.status(200).json(gender);
       } else {
-        res.status(404).json({ error: `Status ${id} not found` });
+        res.status(404).json({ error: `Gender ${id} not found` });
       }
     })
     .catch(next);
 });
 
 router.post('/', requireAdmin, (req, res, next) => {
-  Statuses.createStatus(req.body)
-    .then((newStatus) => {
-      res.status(201).json({ message: 'Status created', status: newStatus });
+  Genders.createGender(req.body)
+    .then((newGender) => {
+      res.status(201).json({ message: 'Gender created', status: newGender });
     })
     .catch(next);
 });
 
 router.put('/:id', requireAdmin, (req, res, next) => {
   const { id } = req.params;
-  Statuses.updateStatus(id, req.body)
-    .then((editedStatus) => {
+  Genders.updateGender(id, req.body)
+    .then((editedGender) => {
       res.status(200).json({
-        message: `Status ${id} updated`,
-        status: editedStatus,
+        message: `Gender ${id} updated`,
+        status: editedGender,
       });
     })
     .catch(next);
@@ -46,12 +46,12 @@ router.put('/:id', requireAdmin, (req, res, next) => {
 
 router.delete('/:id', requireAdmin, (req, res, next) => {
   const { id } = req.params;
-  Statuses.removeStatus(id)
+  Genders.removeGender(id)
     .then((count) => {
       if (count > 0) {
-        res.status(200).json({ message: `Status ${id} has been removed` });
+        res.status(200).json({ message: `Gender ${id} has been removed` });
       } else {
-        res.status(404).json({ message: `Status ${id} could not be found` });
+        res.status(404).json({ message: `Gender ${id} could not be found` });
       }
     })
     .catch(next);
