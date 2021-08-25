ecbca5387f160413873e965125f1dc00d5f57e31
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-24 20:42:38 -0400

    service entry fix

--- api/serviceEntries/serviceEntriesModel.js
+++ api/serviceEntries/serviceEntriesModel.js
@@ -2,71 +2,32 @@ const knex = require('../../data/db-config');
 
 const findAll = async () => {
   return await knex('service_entries as se')
-    .leftJoin('providers as pv', 'se.primary_provider_id', 'pv.provider_id')
-    .leftJoin('recipients as r', 'se.primary_recipient_id', 'r.recipient_id')
-    .leftJoin(
+    .join('providers as pv', 'se.primary_provider_id', 'pv.provider_id')
+    .join('recipients as r', 'se.primary_recipient_id', 'r.recipient_id')
+    .join(
       'service_type_programs as stp',
       'se.service_type_program_id',
       'stp.service_type_program_id'
     )
-    .leftJoin(
-      'service_types as st',
-      'st.service_type_id',
-      'stp.service_type_id'
-    )
-    .leftJoin('programs as pg', 'pg.program_id', 'stp.program_id')
-    .leftJoin('statuses as sta', 'sta.status_id', 'se.status_id')
-    .leftJoin('locations as l', 'l.location_id', 'se.location_id')
+    .join('service_types as st', 'st.service_type_id', 'stp.service_type_id')
+    .join('programs as pg', 'pg.program_id', 'stp.program_id')
+    .join('statuses as sta', 'sta.status_id', 'se.status_id')
+    .join('locations as l', 'l.location_id', 'se.location_id')
     .select(
       knex.raw(
-        'service_type_programs.*, to_json(programs.*) as program, 3, to_json(recipients.*) as recipients'
+        'se.*,  to_json(r.*) as recipient, to_json(st.*) as service_type, to_json(l.*) as location, to_json(pv.*) as provider, to_json(pg.*) as program, to_json(sta.*) as status'
       )
     )
     .groupBy(
-      'stp.service_type_program_id',
-      'p.program_id',
+      'se.service_entry_id',
+      'r.recipient_id',
       'st.service_type_id',
-      'r.recipient_id'
+      'l.location_id',
+      'pv.provider_id',
+      'pg.program_id',
+      'sta.status_id'
     );
 };
-// const findAll = async () => {
-//   return await knex('service_entries as se')
-//     .join('providers as pv', 'se.primary_provider_id', 'pv.provider_id')
-//     .join('recipients as r', 'se.primary_recipient_id', 'r.recipient_id')
-//     .join(
-//       'service_type_programs as stp',
-//       'se.service_type_program_id',
-//       'stp.service_type_program_id'
-//     )
-//     .join('service_types as st', 'st.service_type_id', 'stp.service_type_id')
-//     .join('programs as pg', 'pg.program_id', 'stp.program_id')
-//     .join('statuses as sta', 'sta.status_id', 'se.status_id')
-//     .join('locations as l', 'l.location_id', 'se.location_id')
-//     .select(
-//       'service_entry_id',
-//       'provider_first_name',
-//       'provider_last_name',
-//       'recipient_first_name',
-//       'recipient_last_name',
-//       'program_name',
-//       'service_type_name',
-//       'apply_service_to_household',
-//       'service_date',
-//       'service_time',
-//       'service_duration',
-//       'service_value',
-//       'service_quantity',
-//       'service_entry_notes',
-//       'service_entry_data',
-//       'service_unit_id',
-//       'status',
-//       'service_rating_id',
-//       'l.location_id',
-//       'address',
-//       'se.created_at',
-//       'se.updated_at'
-//     );
-// };
 
 const findById = async (id) => {
   return await knex('service_entries as se')
@@ -82,28 +43,18 @@ const findById = async (id) => {
     .join('statuses as sta', 'sta.status_id', 'se.status_id')
     .join('locations as l', 'l.location_id', 'se.location_id')
     .select(
-      'service_entry_id',
-      'provider_first_name',
-      'provider_last_name',
-      'recipient_first_name',
-      'recipient_last_name',
-      'program_name',
-      'service_type_name',
-      'apply_service_to_household',
-      'service_date',
-      'service_time',
-      'service_duration',
-      'service_value',
-      'service_quantity',
-      'service_entry_notes',
-      'service_entry_data',
-      'service_unit_id',
-      'status',
-      'service_rating_id',
+      knex.raw(
+        'se.*,  to_json(r.*) as recipient, to_json(st.*) as service_type, to_json(l.*) as location, to_json(pv.*) as provider, to_json(pg.*) as program, to_json(sta.*) as status'
+      )
+    )
+    .groupBy(
+      'se.service_entry_id',
+      'r.recipient_id',
+      'st.service_type_id',
       'l.location_id',
-      'address',
-      'se.created_at',
-      'se.updated_at'
+      'pv.provider_id',
+      'pg.program_id',
+      'sta.status_id'
     )
     .where('service_entry_id', id)
     .first();
