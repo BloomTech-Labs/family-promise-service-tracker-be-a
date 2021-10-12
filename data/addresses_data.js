e779791d3092b914ceb774bb3b1e9679774e9e28
davedwight <dpdwight@gmail.com>
2021-10-12 00:35:44 -0600

    replaces address data with addresses with coords, moves helper function to utils

--- data/addresses_data.js
+++ data/addresses_data.js
@@ -7,6 +7,8 @@ const locations = [
     zip: '99021',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.34693187332805,
+    location_latitude: 47.79065449489771,
   },
   {
     address: '2310 N Monroe St',
@@ -16,6 +18,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4255852,
+    location_latitude: 47.67904075,
   },
   {
     address: '222 W Knox Ave',
@@ -25,6 +29,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41499003194618,
+    location_latitude: 47.67722395,
   },
   {
     address: '275-199 E Jackson Ave',
@@ -34,6 +40,8 @@ const locations = [
     zip: '99207',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.40763236786408,
+    location_latitude: 47.68087879117093,
   },
   {
     address: 'E Providence Ave',
@@ -43,6 +51,8 @@ const locations = [
     zip: '99207',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4005648,
+    location_latitude: 47.6923175,
   },
   {
     address: '2426 N Addison St',
@@ -52,6 +62,8 @@ const locations = [
     zip: '99207',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.403377,
+    location_latitude: 47.68011051351351,
   },
   {
     address: '2602 N Hamilton St',
@@ -61,6 +73,8 @@ const locations = [
     zip: '99207',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39657485950875,
+    location_latitude: 47.68155726386303,
   },
   {
     address: '2900 E 1st Ave ',
@@ -70,6 +84,8 @@ const locations = [
     zip: '99202',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.3670146,
+    location_latitude: 47.6562838,
   },
   {
     address: '1204 W Joseph Ave',
@@ -79,6 +95,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42905861301455,
+    location_latitude: 47.709952,
   },
   {
     address: 'Town and Country ',
@@ -88,6 +106,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4216067,
+    location_latitude: 47.7273694,
   },
   {
     address: '300 W Hawthorne Rd',
@@ -97,6 +117,8 @@ const locations = [
     zip: '99251',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4138362,
+    location_latitude: 47.7509565,
   },
   {
     address: '625 E Huron Dr',
@@ -106,6 +128,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.398462,
+    location_latitude: 47.804846,
   },
   {
     address: '1424 S St Charles Rd',
@@ -115,6 +139,8 @@ const locations = [
     zip: '99037',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.20070159171385,
+    location_latitude: 47.643241380307565,
   },
   {
     address: '1423 S Burns Rd',
@@ -124,6 +150,8 @@ const locations = [
     zip: '99037',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.2033464938238,
+    location_latitude: 47.64390492534276,
   },
   {
     address: '4302 S Cook St ',
@@ -133,6 +161,8 @@ const locations = [
     zip: '99223',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.37403399415489,
+    location_latitude: 47.61450094402842,
   },
   {
     address: '4215 S Altamont St',
@@ -142,6 +172,8 @@ const locations = [
     zip: '99223',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.37518078056668,
+    location_latitude: 47.61522070872131,
   },
   {
     address: '12424 N Fairwood Dr',
@@ -151,6 +183,8 @@ const locations = [
     zip: '99218',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42240523132247,
+    location_latitude: 47.770524076293334,
   },
   {
     address: '3404 W Woodland Blvd',
@@ -160,6 +194,8 @@ const locations = [
     zip: '99224',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.462277031246,
+    location_latitude: 47.64426475381127,
   },
   {
     address: '7117 N Division St',
@@ -169,6 +205,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41102214093961,
+    location_latitude: 47.72265966778524,
   },
   {
     address: '7200 N Wall St',
@@ -178,6 +216,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4220292,
+    location_latitude: 47.7224634,
   },
   {
     address: '7905 N Division St',
@@ -187,6 +227,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41108604026846,
+    location_latitude: 47.72965940604027,
   },
   {
     address: '5601 E Sprague Ave',
@@ -196,6 +238,8 @@ const locations = [
     zip: '99212',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.33367262018368,
+    location_latitude: 47.65860045,
   },
   {
     address: '6311 E Sharp Ave',
@@ -205,6 +249,8 @@ const locations = [
     zip: '99212',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.32423301293238,
+    location_latitude: 47.66859675195973,
   },
   {
     address: '10212 E 9th Ave',
@@ -214,6 +260,8 @@ const locations = [
     zip: '99206',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.26782228195206,
+    location_latitude: 47.64867822397067,
   },
   {
     address: '9415 E 4th Ave',
@@ -223,6 +271,8 @@ const locations = [
     zip: '99206',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.27845965079393,
+    location_latitude: 47.65299286637807,
   },
   {
     address: '1441 N Argonne Rd',
@@ -232,6 +282,8 @@ const locations = [
     zip: '99212',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.282919,
+    location_latitude: 47.670109969696966,
   },
   {
     address: '8403 E Broadway Ave',
@@ -241,6 +293,8 @@ const locations = [
     zip: '99212',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.29221672359236,
+    location_latitude: 47.66426117205396,
   },
   {
     address: '9119 E Broadway Ave',
@@ -250,6 +304,8 @@ const locations = [
     zip: '99206',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.2707644,
+    location_latitude: 47.6643005,
   },
   {
     address: '227 W 24th Ave',
@@ -259,6 +315,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41507909357384,
+    location_latitude: 47.63263332246166,
   },
   {
     address: '114 W 27th Ave',
@@ -268,6 +326,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41441079236478,
+    location_latitude: 47.63008230754164,
   },
   {
     address: '911 W 25th Ave',
@@ -277,6 +337,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42525367481747,
+    location_latitude: 47.6316619727444,
   },
   {
     address: '2311 W 16th Ave',
@@ -286,6 +348,8 @@ const locations = [
     zip: '99224',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4814285,
+    location_latitude: 47.6411607,
   },
   {
     address: '1001 S Assembly Rd',
@@ -295,6 +359,8 @@ const locations = [
     zip: '99224',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4750313,
+    location_latitude: 47.6139693,
   },
   {
     address: '322 N Spokane Falls Ct',
@@ -304,6 +370,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41386960428308,
+    location_latitude: 47.66109545,
   },
   {
     address: '333 W Spokane Falls Blvd',
@@ -313,6 +381,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4235233,
+    location_latitude: 47.6601533,
   },
   {
     address: '675 S McClellan St',
@@ -322,6 +392,8 @@ const locations = [
     zip: '99204',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41477248590652,
+    location_latitude: 47.64958773958914,
   },
   {
     address: '3318 W Northwest Blvd',
@@ -331,6 +403,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4600704,
+    location_latitude: 47.6904983,
   },
   {
     address: '165 S Post St,',
@@ -340,6 +414,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42359004318,
+    location_latitude: 47.65470390778936,
   },
   {
     address: '621 W Mallon Avenue',
@@ -349,6 +425,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4221169,
+    location_latitude: 47.6649259,
   },
   {
     address: '10 S Post St',
@@ -358,6 +436,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42361870370164,
+    location_latitude: 47.65703899980406,
   },
   {
     address: '111 S Post St',
@@ -367,6 +447,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42336549999999,
+    location_latitude: 47.6562875,
   },
   {
     address: '1007 W 1st Ave',
@@ -376,6 +458,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4250199,
+    location_latitude: 47.6565591,
   },
   {
     address: '159 S Lincoln St',
@@ -385,6 +469,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4242818,
+    location_latitude: 47.6552258,
   },
   {
     address: '152 S Monroe St',
@@ -394,6 +480,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42679172498472,
+    location_latitude: 47.6552366,
   },
   {
     address: '224 S Lincoln St',
@@ -403,6 +491,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42521770317225,
+    location_latitude: 47.65368515,
   },
   {
     address: '430 W Main Ave',
@@ -412,6 +502,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4193157,
+    location_latitude: 47.6592039,
   },
   {
     address: '808 W. Spokane Falls Blvd.',
@@ -421,6 +513,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4235233,
+    location_latitude: 47.6601533,
   },
   {
     address: '44 W. Riverside Ave',
@@ -430,6 +524,8 @@ const locations = [
     zip: '99201',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4127595428144,
+    location_latitude: 47.658355,
   },
   {
     address: '1001 E. North Foothills Dr.',
@@ -439,6 +535,8 @@ const locations = [
     zip: '99207',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39459520028632,
+    location_latitude: 47.683843249999995,
   },
   {
     address: '6616 N Addison St',
@@ -448,6 +546,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.40283304115776,
+    location_latitude: 47.71768472410941,
   },
   {
     address: '3919 N Maple St',
@@ -457,6 +557,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.43575094631277,
+    location_latitude: 47.6938093,
   },
   {
     address: '1203 W Glass Ave',
@@ -466,6 +568,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.43009,
+    location_latitude: 47.689884,
   },
   {
     address: '3718 N Monroe St',
@@ -475,6 +579,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.425784,
+    location_latitude: 47.69188746999594,
   },
   {
     address: '3222 N Post St',
@@ -484,6 +590,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4232209,
+    location_latitude: 47.692612,
   },
   {
     address: '3315 N Lincoln St',
@@ -493,6 +601,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42521383338008,
+    location_latitude: 47.68808957166724,
   },
   {
     address: '1111 W Dalton Ave',
@@ -502,6 +612,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42844175560498,
+    location_latitude: 47.686983,
   },
   {
     address: '1207 E 38th Ave',
@@ -511,6 +623,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39240218181818,
+    location_latitude: 47.620050121212124,
   },
   {
     address: '1021 W 15th Ave',
@@ -520,6 +634,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.42680769387755,
+    location_latitude: 47.641564,
   },
   {
     address: '618 S Oak St',
@@ -529,6 +645,8 @@ const locations = [
     zip: '99204',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.43874592874286,
+    location_latitude: 47.650206774228394,
   },
   {
     address: '602 N Walnut St',
@@ -538,6 +656,8 @@ const locations = [
     zip: '99206',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.43373257894393,
+    location_latitude: 47.66274821571698,
   },
   {
     address: '1815 N Post St',
@@ -547,6 +667,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.423761,
+    location_latitude: 47.67417342905081,
   },
   {
     address: '1607 W Mansfield Ave',
@@ -556,6 +678,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.43580810207575,
+    location_latitude: 47.677574,
   },
   {
     address: '234 W Park Pl',
@@ -565,6 +689,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.415077375,
+    location_latitude: 47.685445,
   },
   {
     address: '4620 N Stevens St',
@@ -574,6 +700,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41914684739635,
+    location_latitude: 47.700178515110096,
   },
   {
     address: '5725 N Wall St',
@@ -583,6 +711,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4219194,
+    location_latitude: 47.6934754,
   },
   {
     address: '7200 N Wall St',
@@ -592,6 +722,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.4220292,
+    location_latitude: 47.7224634,
   },
   {
     address: '1222 E Winchester Ave',
@@ -601,6 +733,8 @@ const locations = [
     zip: '99218',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39250903824802,
+    location_latitude: 47.761704912750666,
   },
   {
     address: '13619 N Shady Slope Rd',
@@ -610,6 +744,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.37303324672276,
+    location_latitude: 47.780856104829034,
   },
   {
     address: '14709 N Yale Rd',
@@ -619,6 +755,8 @@ const locations = [
     zip: '99021',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.3469317466561,
+    location_latitude: 47.79069198979542,
   },
   {
     address: '3908 E Birch St',
@@ -628,6 +766,8 @@ const locations = [
     zip: '99217',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.35369235362997,
+    location_latitude: 47.721181708129805,
   },
   {
     address: '819 E Rosewood Ave',
@@ -637,6 +777,8 @@ const locations = [
     zip: '99208',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39762355217547,
+    location_latitude: 47.71604062875691,
   },
   {
     address: '5003 N Stevens St',
@@ -646,6 +788,8 @@ const locations = [
     zip: '99205',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.41939712121211,
+    location_latitude: 47.7034646969697,
   },
   {
     address: '3605 N Alberta St',
@@ -655,6 +799,8 @@ const locations = [
     zip: '99205',
     county: ' Spokane County',
     country: 'United States',
+    location_longitude: -117.44932007184748,
+    location_latitude: 47.69136156157879,
   },
   {
     address: '12321 N Pittsburg St',
@@ -664,6 +810,8 @@ const locations = [
     zip: '99218',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.38414424875015,
+    location_latitude: 47.7686325683078,
   },
   {
     address: '1333 E 29th Ave',
@@ -673,6 +821,8 @@ const locations = [
     zip: '99203',
     county: 'Spokane County',
     country: 'United States',
+    location_longitude: -117.39103466308346,
+    location_latitude: 47.62826321883463,
   },
 ];
 
--- data/seeds/013-locations.js
+++ data/seeds/013-locations.js
@@ -1,39 +1,28 @@
 const faker = require('faker/locale/en_US');
 const { getRand, fakeLocationIds } = require('../seedHelpers');
 const { location_types } = require('./012-location_types');
-const getAddressesWithCoords = require('../getAddressesWithCoords');
+const addresses = require('../addresses_data');
 
-const asyncWrapper = async () => {
-  console.log(
-    'FETCHING LOCATION COORDINATES FROM DS API -- SEEDING WILL CONTINUE UPON COMPLETION IN ~2 MINS'
-  );
-  const addresses = await getAddressesWithCoords();
-
-  const locationsArr = [];
-  for (let i = 0; i < addresses.length; i++) {
-    const loc_type_ID = getRand(location_types.length);
-    const location = {
-      location_id: fakeLocationIds[i],
-      location_type_id: loc_type_ID,
-      location_name: faker.lorem.words(),
-      location_description: faker.lorem.sentence(),
-      address: addresses[i].address,
-      address_line2: addresses[i].address_line2,
-      city: addresses[i].city,
-      state: addresses[i].state,
-      zip: addresses[i].zip,
-      county: addresses[i].county,
-      country: addresses[i].country,
-      location_longitude: addresses[i].location_longitude,
-      location_latitude: addresses[i].location_latitude,
-    };
-    locationsArr.push(location);
-  }
-
-  return locationsArr;
-};
-
-const locations = asyncWrapper();
+const locations = [];
+for (let i = 0; i < addresses.length; i++) {
+  const loc_type_ID = getRand(location_types.length);
+  const location = {
+    location_id: fakeLocationIds[i],
+    location_type_id: loc_type_ID,
+    location_name: faker.lorem.words(),
+    location_description: faker.lorem.sentence(),
+    address: addresses[i].address,
+    address_line2: addresses[i].address_line2,
+    city: addresses[i].city,
+    state: addresses[i].state,
+    zip: addresses[i].zip,
+    county: addresses[i].county,
+    country: addresses[i].country,
+    location_longitude: addresses[i].location_longitude,
+    location_latitude: addresses[i].location_latitude,
+  };
+  locations.push(location);
+}
 
 const seed = function (knex) {
   return knex('locations').insert(locations);
