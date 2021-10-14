b8d57e9226e644f5380c6fa1872df0cd98388dfe
michaelquintdev <michaelquintdev@gmail.com>
2021-10-14 13:19:53 -0500

    refactoring for meeting (messy)

--- api/provider_programs/providerProgramsModel.js
+++ api/provider_programs/providerProgramsModel.js
@@ -6,43 +6,43 @@ const findAll = async () => {
     .join('provider_programs as pp', 'p.provider_id', 'pp.provider_id')
     .join('programs as pg', 'pp.program_id', 'pg.program_id')
     .select('p.*', 'pr.provider_role', 'pg.program_name');
-  const dict = {};
-  providerArray.forEach((obj) => {
-    let shapedObj = {
-      provider_id: obj.provider_id,
-      provider_role_id: obj.provider_role_id,
-      employee_id: obj.employee_id,
-      provider_first_name: obj.provider_first_name,
-      provider_last_name: obj.provider_last_name,
-      provider_email: obj.provider_email,
-      provider_phone_number: obj.provider_phone_number,
-      provider_avatar_url: obj.provider_avatar_url,
-      provider_is_active: obj.provider_is_active,
-      created_at: obj.created_at,
-      updated_at: obj.updated_at,
-      provider_role: obj.provider_role,
-      programs: [obj.program_name],
-    };
-    if (!(obj.provider_id in dict)) {
-      dict[obj.provider_id] = shapedObj;
-    } else {
-      dict[obj.provider_id]['programs'].push(obj.program_name);
-    }
-  });
-  const result = [];
-  for (const key in dict) {
-    result.push(dict[key]);
-  }
-  return result;
-};
-
+//   const dict = {};
 //   providerArray.forEach((obj) => {
-//     if (!results.includes(obj.provider_id)) {
-//       results.push(obj.provider_id);
-//     }else{
-
+//     let shapedObj = {
+//       provider_id: obj.provider_id,
+//       provider_role_id: obj.provider_role_id,
+//       employee_id: obj.employee_id,
+//       provider_first_name: obj.provider_first_name,
+//       provider_last_name: obj.provider_last_name,
+//       provider_email: obj.provider_email,
+//       provider_phone_number: obj.provider_phone_number,
+//       provider_avatar_url: obj.provider_avatar_url,
+//       provider_is_active: obj.provider_is_active,
+//       created_at: obj.created_at,
+//       updated_at: obj.updated_at,
+//       provider_role: obj.provider_role,
+//       programs: [obj.program_name],
+//     };
+//     if (!(obj.provider_id in dict)) {
+//       dict[obj.provider_id] = shapedObj;
+//     } else {
+//       dict[obj.provider_id]['programs'].push(obj.program_name);
 //     }
 //   });
+//   const result = [];
+//   for (const key in dict) {
+//     result.push(dict[key]);
+//   }
+//   return result;
+// };
+results = []
+providerArray.forEach((obj) => {
+  if (!results.includes(obj.provider_id)) {
+    results.push(obj.provider_id);
+  }else{
+
+  }
+});
 
 // const findById = async (id) => {
 //   return await knex('service_type_programs as stp')
